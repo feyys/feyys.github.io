ad473d25af21da2d065e0f33e3a39250aeb9fdb7
feyys <111309921+feyys@users.noreply.github.com>
2023-04-10 21:12:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarTexto(){
+    document.getElementById("text").innerHTML ="Gracias por enseñarme a manejar HTML,CSS y JAVASCRIPT";
+}
+function ocultarTexto(){
+    document.getElementById("text").innerHTML =" "
+}
